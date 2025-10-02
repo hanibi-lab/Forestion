a0b63c41729999c4270a73413d99dace4050d2d2
hanibi-lab <leememmie@gmail.com>
2025-10-02 14:04:52 +0900

    home.php 업로드
    섹션 연결할때 우리 파일 이름하고 달라서 오류가 떴네

--- src/home.php
+++ src/home.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
-include_once '../db_connect.php';
+include_once 'db_conn.php';
 
 if(isset($_SESSION['User_Id']) && isset($_SESSION['User_Name'])){
 ?>
